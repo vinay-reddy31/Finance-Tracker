323bfa07c61d53b9d815f8a36e4f685f1df6c89e
vinay_reddy31 <vinayarukala333@gmail.com>
2025-08-06 08:29:41 +0530

    corrected imports

--- utils/getUserId.ts
+++ utils/getUserId.ts
@@ -1,6 +1,6 @@
 // utils/getUserId.ts
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 
 export async function getUserId() {
   const session = await getServerSession(authOptions);
